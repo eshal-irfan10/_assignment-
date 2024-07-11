19074c2a42d96e55f80b0d145e27a481fa7e53b4
Eshal Irfan <155638363+eshal-irfan10@users.noreply.github.com>
2024-07-11 13:31:50 -0700

    Add files via upload

---
+++ assignment 20/script.js
@@ -0,0 +1,74 @@
+// var multiArray = [[]];
+// var multiArray = [[], [], []]; 
+// var multiArray = [ [ [] ], [ [] ], [ [] ] ]; 
+// var matrix = [
+    // [1, 2, 3],
+    // [4, 5, 6],
+    // [7, 8, 9]
+// ];
+// document.write(matrix)
+// for (var i = 1; i <= 10; i++) {
+//     document.write(i + "<br>");
+// }
+// let number = prompt("Enter the number for which you want the multiplication table:");
+// let length = prompt("Enter the length of the multiplication table:");
+
+// number = parseInt(number);
+// length = parseInt(length);
+
+// document.write("<h3>Multiplication Table of " + number + ":</h3>");
+// for (let i = 1; i <= length; i++) {
+//     document.write(number + " x " + i + " = " + (number * i) + "<br>");
+// }
+// let fruits = ["apple","banana","mango","orange","strawberry"];
+// document.write(fruits[0] + "<br>");
+// document.write(fruits[1] + "<br>");
+// document.write(fruits[2] + "<br>");
+// document.write(fruits[3] + "<br>");
+// document.write(fruits[4] + "<br>" + "<br>");
+
+// document.write("Element at index 0 is :" + " " + fruits[0]  + "<br>");
+// document.write("Element at index 1 is :" + " " + fruits[1] + "<br>" );
+// document.write("Element at index 2 is :" + " " + fruits[2]  + "<br>");
+// document.write("Element at index 3 is :" + " " + fruits[3]  + "<br>");
+// document.write("Element at index 4 is :" + " " + fruits[4] + "<br>" );
+
+// document.write("<h1>Counting</h1>")
+// for(let i = 0; i <= 16; i++){
+//     document.write(i + ",")
+// }
+// document.write("<h1>ReverseCounting</h1>")
+// for(let i = 16; i > 0; i--){
+//     document.write(i + ",")
+// }
+// document.write("<h1>Even numbers</h1>")
+// for(let i = 0; i <= 20; i = i + 2 ){
+//     document.write(i + ",")
+// }
+// document.write("<h1>Odd numbers</h1>")
+// for(let i = 1; i <=19; i = i + 2){
+//     document.write(i + ",")
+// }
+// document.write("<h1>Series: </h1>");
+// for (let i = 1; i <= 10; i++) {
+//     document.write((2 * i) + "k");
+//     if (i < 10) {
+//         document.write(", ");
+//     }
+// // // }
+// 
+// let a = [24, 53, 78, 91, 12];
+// let b = Math.max(...a);;
+// document.write("Array items :" + a + "<br>");
+// document.write("The largest number is  :" + b );
+// let a = [24, 53, 78, 91, 12];
+//  let b = Math.min(...a);;
+//  document.write("Array items :" + a + "<br>");
+//  document.write("The smallest number is  :" + b );
+// let Multiplication = 5
+// for(let i = 0; i <= 100; i++){
+//     if(i % 5 === 0)
+//     {
+//         document.write(i + "<br>")
+//     }
+// }
